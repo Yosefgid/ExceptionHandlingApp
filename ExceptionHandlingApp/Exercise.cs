a9abdbbe6171e96cc8626cb18a8c318fe50342a3
Yosefgid <jossykasshun@yahoo.com>
2026-03-25 11:35:55 +0000

    Add an Exercise Class

---
+++ ExceptionHandlingApp/Exercise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingApp
+{
+    internal class Exercise1
+    {
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+        public static void DivideUserInputs()
+        {
+            while (true)
+            {
+            }
+        }
+        private static int[] GetUserInputs()
+        {
+
+        }
+    }
+}
