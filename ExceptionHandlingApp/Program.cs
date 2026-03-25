dbd29766b176c1e2357e4068c18a65d3f3d47afb
Yosefgid <jossykasshun@yahoo.com>
2026-03-25 11:26:12 +0000

    Add project files.

---
+++ ExceptionHandlingApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExceptionHandlingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
